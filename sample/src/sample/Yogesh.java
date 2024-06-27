8e4b7f1273963ccf33fa80192d327a7446cc14c1
Asus <Asus@LAPTOP-AC0CJGAI>
2024-06-27 11:02:41 +0530

    This is my first core project.

---
+++ sample/src/sample/Yogesh.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Yogesh {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JFS");
+		System.out.println("MVIT");
+		System.out.println("TNSIF");
+	}
+
+}
