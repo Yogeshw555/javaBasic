071ac90bb57610afb92c3a0113e9adf133fee270
Asus <Asus@LAPTOP-AC0CJGAI>
2024-07-01 09:19:19 +0530

    Day 3 - student java

---
+++ sample/src/day3/Student.java
@@ -0,0 +1,35 @@
+package day3;
+
+public class Student {
+
+	
+	    int sid;
+	    String sname;
+	    String city;
+	    
+	    public void setSid(int sid) {
+	    	this.sid=sid;
+	    }
+	    public int getSid()
+	    {
+	    	return sid;
+	    }
+	    public String getSname() {
+	    	return sname;
+	    }
+	    public void setSname(String sname) {
+	    	this.sname=sname;
+	    }
+	    public void setSname(String city) {
+	    	this.city=city;
+	    }
+		@Override
+		public String toString() {
+			return "Student [sid=" + sid + ", sname=" + sname + ", city=" + city + "]";
+		}
+	    
+	    	
+	    
+	
+
+}
---
+++ sample/src/day3/StudentDemo.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class StudentDemo {
+	public static void main(String[]args) {
+		Student s1=new Student();
+		s1.sid=101;
+		s1.city="puducherry";
+	
+	     
+	    System.out.println("Student id:"+s1.sid);
+	    System.out.println("Student id:"+s1.sname);
+	    System.out.println("Student id:"+s1.city);
+    }
+}
